dca6c5bb7b8a0ae1b44ff8ed5eaaccf539a78eeb
A01254093ASML <112582790+A01254093ASML@users.noreply.github.com>
2022-08-31 12:39:08 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-basic.showLeds(`
-    . . # . .
-    # # # # #
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     basic.showIcon(IconNames.Yes)
     basic.showIcon(IconNames.No)
